0c909d6b582e326a9e6805f0b34584be0d69b2e3
ozgurozbekuk <ozgurozbek.uk@gmail.com>
2025-09-05 15:19:46 +0100

    auth controller

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js"
 import connectMongoDB from "./db/connectMongo.js";
-
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+
 
 dotenv.config()
 
@@ -10,7 +11,12 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+//middleware
+app.use(express.json()) //parse req.body
+app.use(express.urlencoded({extended:true})) //parse form data
+app.use(cookieParser())
 app.use('/api/auth',authRoutes)
+
 
 
 

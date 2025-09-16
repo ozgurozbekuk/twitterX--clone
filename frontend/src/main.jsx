9a71f04ed13b65840a6e8477e227492754e1dfcb
ozgurozbekuk <ozgurozbek.uk@gmail.com>
2025-09-16 09:58:48 +0100

    create post

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,16 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
-  defaultOptions:{
-    queries:{
-      refetchOnWindowFocus:false
-    }
-  }
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      queryFn: async ({ queryKey }) => {
+        const res = await fetch(`/api/${queryKey[0]}`);
+        if (!res.ok) throw new Error("Network error");
+        return res.json();
+      },
+    },
+  },
 });
 
 createRoot(document.getElementById("root")).render(
